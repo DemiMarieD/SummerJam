60cdfc7e5ca156abcf988319d9cd9bdf92d0040f
DemiMarieD <demi-marie@outlook.de>
2017-07-12 10:23:46 +0200

    First Test in Cipher Fragment

--- app/src/main/java/com/example/demi/summerjam/CipherFragment.java
+++ app/src/main/java/com/example/demi/summerjam/CipherFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Demi on 12.07.2017.
@@ -13,12 +15,25 @@ import android.view.ViewGroup;
 
 public class CipherFragment extends Fragment{
 
+    TextView titel;
+    Button click;
     View myCipher;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myCipher = inflater.inflate(R.layout.cipher_layout, container, false);
+
+        click = (Button) myCipher.findViewById(R.id.click_b);
+        titel = (TextView) myCipher.findViewById(R.id.titel_tv);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                titel.setText("clicked");
+            }
+        });
+
         return myCipher;
     }
 }
