db3503a348757c1fb45f870616366d57d42f97ba
Mrabiea <rabeesy88@gmail.com>
2017-07-12 11:28:58 +0200

    Cipher feature

--- app/src/main/java/com/example/demi/summerjam/CipherFragment.java
+++ app/src/main/java/com/example/demi/summerjam/CipherFragment.java
@@ -3,11 +3,16 @@ package com.example.demi.summerjam;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
 
 /**
  * Created by Demi on 12.07.2017.
@@ -15,22 +20,57 @@ import android.widget.TextView;
 
 public class CipherFragment extends Fragment{
 
-    TextView titel;
-    Button click;
+    TextView ceasar;
+    Button cipher;
     View myCipher;
+    EditText key;
+    EditText inputText;
+    String finalText;
+    Integer number;
+    Integer length;
+    int result;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myCipher = inflater.inflate(R.layout.cipher_layout, container, false);
 
-        click = (Button) myCipher.findViewById(R.id.click_b);
-        titel = (TextView) myCipher.findViewById(R.id.titel_tv);
+        cipher = (Button) myCipher.findViewById(R.id.cipher);
+        ceasar = (TextView) myCipher.findViewById(R.id.Ceasar);
+        key=(EditText)myCipher.findViewById(R.id.Key) ;
+        inputText=(EditText)myCipher.findViewById(R.id.inputText) ;
 
-        click.setOnClickListener(new View.OnClickListener() {
+        cipher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                titel.setText("clicked");
+                finalText = "Solution: ";
+                number = Integer.parseInt(key.getText().toString());
+
+                if(number >= 26){
+                    number = number%26;
+                }
+                length= inputText.length();
+                Editable decode = inputText.getText();
+
+                for (int i=0; i < length; i++ ){
+                    Character letter = decode.charAt(i);
+                    result = (int)letter+ number;
+
+                    if( isUpperCase(letter) && (result > 'Z')){
+                        result = result -26;
+                    }else if( isLowerCase(letter) && (result > 'z')){
+                        result = result -26;
+                    }
+
+                    if(Character.isLetter(letter)){
+                        finalText = finalText + (char) result;
+                    }else {
+                        finalText = finalText + letter;
+                    }
+
+                }
+
+                ceasar.setText(finalText);
             }
         });
 
